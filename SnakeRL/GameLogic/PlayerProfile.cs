cf058d2cec3028a19e8d9087df0c853b36b5f92f
alexgold25 <alexandrgoldenbergg@gmail.com>
2025-08-14 00:56:59 +0500

    Add player profile system model and data?

---
+++ SnakeRL/GameLogic/PlayerProfile.cs
@@ -0,0 +1,10 @@
+namespace SnakeRL.GameLogic
+{
+    public class PlayerProfile
+    {
+        public string Name { get; set; } = "";
+        public int GamesPlayed { get; set; }
+        public int BestLength { get; set; }
+    }
+}
+
